795d51f47235c6690aac8cbccbaf80bbda6ccd02
Nathan <nathan.cox.cb@gmail.com>
2023-04-02 00:24:40 +1300

    Init

---
+++ src/components/App.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+    import File from "./File.svelte";
+
+    let folders = [];
+    for (let i = 0; i < 6; i++) {
+        folders.push({
+            name: "Folder",
+            type: "folder",
+            selected: i % 2,
+        });
+    }
+
+    let files = [];
+    for (let i = 0; i < 20; i++) {
+        files.push({
+            name: "File name",
+            type: "png",
+            thumbnail:
+                "https://coxy.tech/jw__pillars-of-creation--1920x1080.jpg",
+            selected: i % 2,
+        });
+    }
+</script>
+
+<header class="header">
+    <h2>C-Drive</h2>
+</header>
+
+<main>
+    <div class="grid">
+        {#each folders as folder}
+            <File file={folder} />
+        {/each}
+    </div>
+
+    <div class="grid">
+        {#each files as file}
+            <File {file} />
+        {/each}
+    </div>
+</main>
---
+++ src/components/File.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+    import { fade } from "svelte/transition";
+
+    export let file;
+
+    let options = false,
+        optionList: HTMLElement;
+
+    async function openOptions() {
+        options = true;
+        await optionList;
+        optionList.focus();
+    }
+
+    function closeOptions() {
+        options = false;
+    }
+
+    function deleteFile() {}
+</script>
+
+<div class="file" class:file--selected={file.selected}>
+    {#if file.type != "folder"}
+        <div
+            class="file__thumbnail"
+            style="background-image: url({file.thumbnail});"
+        />
+    {/if}
+    <div class="file__title">{file.name}</div>
+    <button class="file__option-button" on:click={openOptions} />
+    {#if options}
+        <div
+            class="file__option-list"
+            tabindex="-1"
+            transition:fade={{ duration: 100 }}
+            bind:this={optionList}
+            on:focusout={closeOptions}
+            on:keydown={(e) => {
+                if (e.key == "Escape") closeOptions();
+            }}
+        >
+            {#if file.type != "folder"}
+                <a
+                    class="file__option"
+                    href={file.thumbnail}
+                    target="_blank"
+                    download
+                >
+                    Download
+                </a>
+            {/if}
+            <div
+                class="file__option"
+                on:click={deleteFile}
+                on:keydown={deleteFile}
+            >
+                Delete
+            </div>
+        </div>
+    {/if}
+</div>
